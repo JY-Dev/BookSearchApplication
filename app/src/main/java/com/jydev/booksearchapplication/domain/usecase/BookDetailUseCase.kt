1a7ab457449840f055dd9520fed81ac0aa8e997c
JY-Dev <kjy0302014@gmail.com>
2021-12-14 21:49:17 +0900

    [UPDATE] BookDetailUseCase NetworkResult로 반환하도록 변경

--- app/src/main/java/com/jydev/booksearchapplication/domain/usecase/BookDetailUseCase.kt
+++ app/src/main/java/com/jydev/booksearchapplication/domain/usecase/BookDetailUseCase.kt
@@ -1,9 +1,17 @@
 package com.jydev.booksearchapplication.domain.usecase
 
 import com.jydev.booksearchapplication.domain.model.BookDetail
+import com.jydev.booksearchapplication.domain.model.NetworkResult
 import com.jydev.booksearchapplication.domain.repository.BookRepository
+import java.lang.Exception
 
 class BookDetailUseCase(private val bookRepository: BookRepository) {
-    suspend operator fun invoke(bookId : String) : BookDetail =
-        bookRepository.getBookDetail(bookId)
+    suspend operator fun invoke(bookId : String) : NetworkResult<BookDetail> {
+        return try {
+            val data = bookRepository.getBookDetail(bookId)
+            NetworkResult.Success(data)
+        } catch (e : Exception){
+            NetworkResult.Error(e.message?:"NetWork Error")
+        }
+    }
 }
