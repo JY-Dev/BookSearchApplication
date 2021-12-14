1457ab14818bb0cf187c16b851a54323e3117e4b
JY-Dev <kjy0302014@gmail.com>
2021-12-14 18:37:11 +0900

    [UPDATE] MainActivity에 searchBooksViewModel 생성 ,AndroidEntryPoint 지정, SearchBooksAdapter 생성, initView initListener 추가

--- app/src/main/java/com/jydev/booksearchapplication/ui/MainActivity.kt
+++ app/src/main/java/com/jydev/booksearchapplication/ui/MainActivity.kt
@@ -2,11 +2,44 @@ package com.jydev.booksearchapplication.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.jydev.booksearchapplication.R
+import androidx.activity.viewModels
+import com.jydev.booksearchapplication.databinding.ActivityMainBinding
+import com.jydev.booksearchapplication.ui.adapter.SearchBooksAdapter
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val searchBooksViewModel : SearchBooksViewModel by viewModels()
+    private val searchBooksAdapter : SearchBooksAdapter by lazy {
+        SearchBooksAdapter()
+    }
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initView()
+        initListener()
     }
+
+    private fun initView() =
+        with(binding){
+            searchBooksRecyclerview.adapter = searchBooksAdapter
+        }
+
+    private fun initListener() =
+        with(binding){
+            searchButton.setOnClickListener {
+                val query = searchEditText.text.toString()
+                searchBooksViewModel.searchBooks(query).observe(this@MainActivity){
+                    CoroutineScope(Dispatchers.Main).launch {
+                        searchBooksAdapter.submitData(it)
+                    }
+                }
+            }
+        }
+
 }
