b8cc81b8cee6be2dce4ee98b5de809b6e0a14e30
JY-Dev <kjy0302014@gmail.com>
2021-12-14 21:36:14 +0900

    [UPDATE] SearchBooksUseCase는 생성자 주입이 필요없기때문에 @Inject 제거

--- app/src/main/java/com/jydev/booksearchapplication/domain/usecase/SearchBooksUseCase.kt
+++ app/src/main/java/com/jydev/booksearchapplication/domain/usecase/SearchBooksUseCase.kt
@@ -2,9 +2,8 @@ package com.jydev.booksearchapplication.domain.usecase
 
 import com.jydev.booksearchapplication.domain.model.Book
 import com.jydev.booksearchapplication.domain.repository.BookRepository
-import javax.inject.Inject
 
-class SearchBooksUseCase @Inject constructor(private val bookRepository: BookRepository) {
+class SearchBooksUseCase(private val bookRepository: BookRepository) {
     suspend operator fun invoke(query : String, page : Int) : List<Book>{
         return bookRepository.searchBooks(query, page)
     }
