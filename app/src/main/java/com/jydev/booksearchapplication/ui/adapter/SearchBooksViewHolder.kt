100a811a916e3b79efbc43139555972fc3899e1a
JY-Dev <kjy0302014@gmail.com>
2021-12-14 19:20:40 +0900

    [UPDATE] item_search_books book_image_view , subtitle_text_view 추가

--- app/src/main/java/com/jydev/booksearchapplication/ui/adapter/SearchBooksViewHolder.kt
+++ app/src/main/java/com/jydev/booksearchapplication/ui/adapter/SearchBooksViewHolder.kt
@@ -1,13 +1,18 @@
 package com.jydev.booksearchapplication.ui.adapter
 
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.jydev.booksearchapplication.databinding.ItemSearchBooksBinding
 import com.jydev.booksearchapplication.domain.model.Book
 
 class SearchBooksViewHolder(private val binding : ItemSearchBooksBinding) : RecyclerView.ViewHolder(binding.root) {
     fun bind(book : Book?){
         with(binding){
+            Glide.with(binding.root.context)
+                .load(book?.image)
+                .into(bookImageView)
             titleTextView.text = book?.title
+            subTitleTextView.text = book?.subtitle
         }
     }
 }
