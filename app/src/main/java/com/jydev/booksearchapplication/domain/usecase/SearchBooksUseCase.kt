f0bf990308e514734fc8502aed0a06638f77b1dc
JY-Dev <kjy0302014@gmail.com>
2021-12-14 14:25:30 +0900

    [UPDATE] SearchBooksListUseCase -> SearchBooksUseCase 로 변경

--- app/src/main/java/com/jydev/booksearchapplication/domain/usecase/SearchBooksUseCase.kt
+++ app/src/main/java/com/jydev/booksearchapplication/domain/usecase/SearchBooksUseCase.kt
@@ -4,7 +4,7 @@ import com.jydev.booksearchapplication.domain.model.Book
 import com.jydev.booksearchapplication.domain.repository.BookRepository
 import javax.inject.Inject
 
-class SearchBooksListUseCase @Inject constructor(private val bookRepository: BookRepository) {
+class SearchBooksUseCase @Inject constructor(private val bookRepository: BookRepository) {
     suspend operator fun invoke(query : String, page : Int) : List<Book>{
         return bookRepository.searchBooks(query, page)
     }
