f747c3dd290dba403af7b478f8109e67c0bf9bbb
JY-Dev <kjy0302014@gmail.com>
2021-12-14 21:30:20 +0900

    [ADD] BookDetail 추가

---
+++ app/src/main/java/com/jydev/booksearchapplication/domain/model/BookDetail.kt
@@ -0,0 +1,14 @@
+package com.jydev.booksearchapplication.domain.model
+
+data class BookDetail(
+    val authors: String,
+    val description: String,
+    val error: String,
+    val image: String,
+    val price: String,
+    val publisher: String,
+    val rating: String,
+    val subtitle: String,
+    val title: String,
+    val url: String
+)
