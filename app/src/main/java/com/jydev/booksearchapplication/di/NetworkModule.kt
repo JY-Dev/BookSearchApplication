88541fc5ffa80540de079644501ad2f72a670f76
JY-Dev <kjy0302014@gmail.com>
2021-12-14 13:41:10 +0900

    [UPDATE] NetworkModule Module 어노테이션 추가

--- app/src/main/java/com/jydev/booksearchapplication/di/NetworkModule.kt
+++ app/src/main/java/com/jydev/booksearchapplication/di/NetworkModule.kt
@@ -1,13 +1,18 @@
 package com.jydev.booksearchapplication.di
 
 import com.jydev.booksearchapplication.remote.service.BookService
+import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
     private const val BASE_URL = "https://api.itbook.store/"
 
